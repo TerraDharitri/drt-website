8d56c5009b0b3026596831e45e53d65331585ee5
Donsudhir <sudhirsudesh1962002@gmail.com>
2025-02-04 11:23:12 +0530

    check

--- src/components/sections/ProductsSection.jsx
+++ src/components/sections/ProductsSection.jsx
@@ -118,7 +118,7 @@ const ProductsSection = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-            Our Transaction
+            Our Transactions
           </h2>
           <p className="mt-4 text-xl text-gray-500">
             Become part of a community of millions.
