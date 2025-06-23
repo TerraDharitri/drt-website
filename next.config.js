1c53b2ce48c391bf51afa3de9e3123c4ec489464
Donsudhir <sudhirsudesh1962002@gmail.com>
2025-06-23 17:19:41 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Essential for static export
+  basePath: isProd ? '/pRewa_website' : '', // Adjust based on your repo name
+  assetPrefix: isProd ? '/pRewa_website/' : '', // Adjust based on your repo name
   images: {
-    unoptimized: true          // disables _next/image for static hosting
+    unoptimized: true, // <-- This is key for GitHub Pages
   },
-  basePath: isGithubPages ? '/pRewa_website' : '',
-  assetPrefix: isGithubPages ? '/pRewa_website/' : '',
 };
 
 module.exports = nextConfig;
-
-
-
-
