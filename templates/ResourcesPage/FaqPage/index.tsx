c88db5555860c9962f92e8134a11fe4fab26bb6b
Donsudhir <sudhirsudesh1962002@gmail.com>
2025-06-24 10:14:16 +0530

    updated faq and homepage

--- templates/ResourcesPage/FaqPage/index.tsx
+++ templates/ResourcesPage/FaqPage/index.tsx
@@ -24,7 +24,7 @@ const FaqPage = ({}: FaqPageProps) => {
         </div>
       </div>
       <div className="container flex md:block">
-        <div className="shrink-0 w-[16.25rem] lg:w-44 md:flex md:w-auto md:mb-6 md:overflow-auto md:scroll-smooth md:scrollbar-none md:-mx-6 md:before:shrink-0 md:before:w-6 md:after:shrink-0 md:after:w-6">
+        {/* <div className="shrink-0 w-[16.25rem] lg:w-44 md:flex md:w-auto md:mb-6 md:overflow-auto md:scroll-smooth md:scrollbar-none md:-mx-6 md:before:shrink-0 md:before:w-6 md:after:shrink-0 md:after:w-6">
           {faq.map((button) => (
             <button
               className={`flex w-full p-3 border border-transparent text-lg font-medium transition-colors hover:text-greyscale-900 dark:hover:text-dark-text-primary md:shrink-0 md:w-auto ${
@@ -38,7 +38,7 @@ const FaqPage = ({}: FaqPageProps) => {
               {button.title}
             </button>
           ))}
-        </div>
+        </div> */}
         <div className="grow pl-30 xl:pl-20 lg:pl-10 md:pl-0">
           {faq
             .filter((x: any) => x.id === activeId)
